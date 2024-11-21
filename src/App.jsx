80e543bc74693d284f6e571fa852b89a93ac2f8b
jeferson santos <jefersondeoliveira062@gmail.com>
2024-11-21 15:41:32 -0300

    commit initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-green-800'>
+      Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
